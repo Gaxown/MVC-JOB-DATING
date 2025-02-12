cf135ed2255ea3fc5c11020b676d1246e041c9fd
soaud arziki <arziki.souad@gmail.com>
2025-02-12 15:49:11 +0100

    qsdfghjk

--- app/models/Company.php
+++ app/models/Company.php
@@ -7,27 +7,6 @@ use App\Models\Announcement;
 class Company extends Model {
     protected $table = "companies";
     protected $fillable = ['name','logo','cover','description','website','service','effective','location','capital'];
-    
-    // public static function showCompanies(){
-    //     $companies = Model::all();
-    //     return $companies;
-    // }
-
-    // public function createCompany($name,$logo,$cover,$description,$website,$service,$effective,$location,$capital){
-    //     $company = [
-    //         'name' => $name,
-    //         'logo' => $logo,
-    //         'cover' => $cover,
-    //         'description' => $description,
-    //         'website' => $website,
-    //         'service' => $service,
-    //         'effective' => $effective,
-    //         'location' => $location,
-    //         'capital' => $capital,
-    //     ];
-        
-    //     return $newCompany = Model::create($company);
-    // }
 
     public function announcements(){
         $this->hasMany(Announcement::class);
