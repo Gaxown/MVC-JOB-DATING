26fd58f57f0c8ed99a5126cc1b91a4af07768646
ayoub oumha <ayyouboumha@gmail.com>
2025-02-15 18:40:08 +0100

    commit announcements add updat delet

--- app/controllers/AnnouncementsController.php
+++ app/controllers/AnnouncementsController.php
@@ -46,7 +46,7 @@ class AnnouncementsController extends Controller
             'title' => ['required'],
             'company_id' => ['required', 'numeric'],
             'candidates_count' => ['required', 'numeric'],
-            'cover' => ['required', 'image', 'size:5000'],
+            // 'cover' => ['required', 'image', 'size:5000'],
             'description' => ['required'],
         ];
     
@@ -155,20 +155,58 @@ class AnnouncementsController extends Controller
 
     public function editForm($id)
     {
+        $companies = Company::getAllCompanies();
         $announcement = Announcement::find($id);
-        return View::render('announcements/update', compact('announcement'));
+        return View::render('admin/announcements/update', compact('announcement', 'companies'));
+      
     }
 
     public function update($id)
-    {
-        Announcement::updateOrCreate($id, [
-            'title' => $_POST['title'],
-            'company_id' => Validator::validate('company_id', 'required|numeric'),
-            'candidates_count' => Validator::validate('candidates_count', 'required|numeric'),
-            'cover' => Validator::validate('cover', 'required|image|size:5000'),
-            'description' => Validator::validate('description', 'required'),
-        ]);
-        header('Location: /announcements');
+    {    $errors = [];
+        foreach ($_POST as $key => $value) {
+            $_POST[$key] = Security::clean($value);
+        }
+    
+        $validations = [
+            'title' => ['required'],
+            'company_id' => ['required', 'numeric'],
+            'candidates_count' => ['required', 'numeric'],
+            // 'cover' => ['required', 'image', 'size:5000'],
+            'description' => ['required'],
+        ];
+    
+        foreach ($validations as $field => $rules) {
+            if (!isset($_POST[$field]) || !Validator::validate($_POST[$field], implode('|', $rules))) {
+                $errors[$field] = Validator::getErrors()[$field] ?? ['This field is required'];
+            }
+        }
+    
+        $coverPath = '';
+        if (empty($errors)) {
+            try {
+                if (isset($_FILES['cover']) && $_FILES['cover']['size'] > 0) {
+                    $coverPath = $this->handleFileUpload($_FILES['cover'], 'covers');
+                }
+                $announcement = [
+                    'title' => $_POST['title'],
+                    'company_id' => $_POST['company_id'],
+                    'candidates_count' => $_POST['candidates_count'],
+                    'cover' => $coverPath,
+                    'description' => $_POST['description'],
+                ];
+             
+              
+                // Announcement::updateOrCreate($id , $announcement);
+                Announcement::where('id', $id)->update($announcement);
+                header('Location: /admin/announcements');
+                exit();
+            } catch (\Exception $e) {
+                $errors['file'] = [$e->getMessage()];
+            }
+        }
+    
+        return View::render('admin/announcements/update', ['errors' => $errors]);
+      
         exit();
     }
 
--- routes/routes.php
+++ routes/routes.php
@@ -20,7 +20,7 @@ $r->get('/admin/announcements', [AnnouncementsController::class, 'index']);
 $r->get('/admin/announcements/create', [AnnouncementsController::class, 'createForm']);
 $r->post('/admin/announcements/store', [AnnouncementsController::class, 'store']);
 $r->get('/admin/announcements/details/{id}', [AnnouncementsController::class, 'show']);
-$r->get('/admin/announcements/update/{id}', [AnnouncementsController::class, 'updateForm']);
+$r->get('/admin/announcements/update/{id}', [AnnouncementsController::class, 'editForm']);
 $r->post('/admin/announcements/update/{id}', [AnnouncementsController::class, 'update']);
 $r->get('/admin/announcements/delete/{id}', [AnnouncementsController::class, 'softDeleteAnnouncement']);
 
